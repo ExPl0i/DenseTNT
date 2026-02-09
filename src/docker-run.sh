dccb890e6558e224b8a265cb096a64c6fbe565e5
Yuriy Porokhin <it@yporokhin.ru>
2026-02-09 11:15:07 +0700

    Add docker support

---
+++ src/docker-run.sh
@@ -0,0 +1,10 @@
+docker run -d --name densentn \
+  --gpus '"device=0,2"' \
+  --ipc=host --shm-size=8g \
+  --ulimit memlock=-1 --ulimit stack=67108864 \
+  --cap-add=IPC_LOCK \
+  -v /mnt/scsi_nas/waymo_datasets:/data/waymo \
+  -v /mnt/scsi_nas/env-predict/preprocessed:/data/preprocessed \
+  -v /mnt/scsi_nas/env-predict/checkpoints:/data/checkpoints \
+  expl0i/densetnt:cu118 \
+  tail -f /dev/null
