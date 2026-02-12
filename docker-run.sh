54c0f8e546a69879fb61f85076ed27d0babe6e82
Yuriy Porokhin <it@yporokhin.ru>
2026-02-12 10:25:47 +0700

    fix

--- docker-run.sh
+++ docker-run.sh
@@ -3,7 +3,7 @@ docker run -d --name densentn \
   --ipc=host --shm-size=8g \
   --ulimit memlock=-1 --ulimit stack=67108864 \
   --cap-add=IPC_LOCK \
-  -v /mnt/scsi_nas/waymo_datasets:/data/waymo \
+  -v /mnt/scsi_nas/waymo_datasets/motion_v_1_3_1/uncompressed/tf_example/training:/data/train \
   -v /mnt/scsi_nas/env-predict/preprocessed:/data/preprocessed \
   -v /mnt/scsi_nas/env-predict/checkpoints:/data/checkpoints \
   expl0i/densetnt:cu118 \
